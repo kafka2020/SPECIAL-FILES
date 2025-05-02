6bff896ec0fa2db2d0c2bd0321afcd3b3f26303f
Andrew S <>
2025-05-02 18:49:39 +0300

    fit: implement methods listToJson, writeString

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.opencsv.*;
 import com.opencsv.bean.*;
 import com.opencsv.exceptions.*;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.*;
 
 public class Main {
@@ -10,6 +14,26 @@ public class Main {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        System.out.println(json);
+        writeString(json, "data.json");
+    }
+
+    private static void writeString(String json, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(json);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        String json = gson.toJson(list, listType);
+        return json;
     }
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -27,7 +51,7 @@ public class Main {
 
         } catch (IOException e) {
             e.printStackTrace();
+            return Collections.emptyList(); // Возвращаем пустой список в случае ошибки
         }
-        return Collections.emptyList(); // Возвращаем пустой список в случае ошибки
     }
 }
